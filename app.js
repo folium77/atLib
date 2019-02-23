c34a8f94569def8b13f45bd700da50202f3795fa
kiyo <>
2019-02-23 23:31:00 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 const func = require('./function.js');
 
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://127.0.0.1:27017/";
-
 app.get('/', (req, res) => {
 
   const isbn = req.query.isbn;
