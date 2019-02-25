7baabdae96b48d5b094bad1c8c46f76f421ccc59
kiyo <>
2019-02-26 00:45:42 +0900

    Update function.js

--- function.js
+++ function.js
@@ -24,41 +24,8 @@ exports.getBookNDC = (isbn, data) => {
       if (err) {
         reject(err);
       }
-
-      /*
-        ・新刊はNDL9がない（国会図書館にない）場合があるので対策
-        ・書影がない場合の対策（自分で入力できるようにする）
-        ・フリガナが取れないものもある
-      */
-      const book = data[0].summary;
-      const contributor = alphabetCheck(data[0].onix.DescriptiveDetail.Contributor);
-      const personName = contributor.PersonName;
-      const author = personName.content;
-      const author_kana = (personName.collationkey) ? personName.collationkey : '';
-      const pubdate = new Date(book.pubdate.replace(/^(\d{4})(\d{2})(\d{2})/,'$1/$2/$3'));
-      const supplyDetail = data[0].onix.ProductSupply.SupplyDetail.Price;
-      const price = (supplyDetail) ? supplyDetail[0].PriceAmount : '';
-      const cat = require('./ndc9.json');
-      const $ = cheerio.load(res.text);
-      const ndl8 = $('dc\\:subject[xsi\\:type="dcndl:NDC8"]').eq(0).text();
-      const ndl9 = $('dc\\:subject[xsi\\:type="dcndl:NDC9"]').eq(0).text();
-      const ndl = (ndl9) ? ndl9 : ndl8;
-      const bookInfo = {
-        'isbn'        : book.isbn,
-        'title'       : book.title,
-        'author'      : author,
-        'author_kana' : author_kana,
-        'publisher'   : book.publisher,
-        'pub_date'    : pubdate,
-        'price'　　　　: price,
-        'cover'       : book.cover,
-        'ndl'        : ndl,
-        'category'    : cat[ndl.slice(0,3)],
-        'post_date'   : new Date()
-      }
-
+      const bookInfo = createBookInfo(data, res);
       resolve(bookInfo);
-
     });
   });
 };
@@ -72,6 +39,41 @@ const alphabetCheck = (contributors) => {
     }
   });
   return result[0];
+};
+
+// 書籍情報の配列を作成
+const createBookInfo = (data, res) => {
+  /*
+    ・新刊はNDL9がない（国会図書館にない）場合があるので対策
+    ・書影がない場合の対策（自分で入力できるようにする）
+    ・フリガナが取れないものもある
+  */
+  const book = data[0].summary;
+  const contributor = alphabetCheck(data[0].onix.DescriptiveDetail.Contributor);
+  const personName = contributor.PersonName;
+  const author = personName.content;
+  const author_kana = (personName.collationkey) ? personName.collationkey : '';
+  const pubdate = new Date(book.pubdate.replace(/^(\d{4})(\d{2})(\d{2})/,'$1/$2/$3'));
+  const supplyDetail = data[0].onix.ProductSupply.SupplyDetail.Price;
+  const price = (supplyDetail) ? supplyDetail[0].PriceAmount : '';
+  const cat = require('./ndc9.json');
+  const $ = cheerio.load(res.text);
+  const ndl8 = $('dc\\:subject[xsi\\:type="dcndl:NDC8"]').eq(0).text();
+  const ndl9 = $('dc\\:subject[xsi\\:type="dcndl:NDC9"]').eq(0).text();
+  const ndl = (ndl9) ? ndl9 : ndl8;
+  return {
+    'isbn'        : book.isbn,
+    'title'       : book.title,
+    'author'      : author,
+    'author_kana' : author_kana,
+    'publisher'   : book.publisher,
+    'pub_date'    : pubdate,
+    'price'　　　　: price,
+    'cover'       : book.cover,
+    'ndl'        : ndl,
+    'category'    : cat[ndl.slice(0,3)],
+    'post_date'   : new Date()
+  }
 };
 
 // SELECT DB
